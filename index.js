jonschlinkert/benchmarked

commit 89e721a70e7d976c6a3023740887edad5a0b859b

always apply args

index.js
@@ -78,7 +78,7 @@ Benchmarked.prototype.toFile = function(type, filepath, options) {
 
   file.key = utils.setKey(file, opts);
   file.inspect = function() {
-    return `<${utils.toTitle(type)} ${this.key} "${this.relative}">`;
+    return '<' + utils.toTitle(type) + ' ' + this.key +  '"' + this.relative + '">';
   };
 
   var fn = opts.toFile || this[type].toFile;
@@ -294,12 +294,7 @@ Benchmarked.prototype.addSuite = function(fixture) {
         cursor.write('  ' + event.target);
       },
       fn: function() {
-        var args = fixture.content;
-        if (Array.isArray(args[0])) {
-          file.run.apply(null, args);
-        } else {
-          file.run(args);
-        }
+        file.run.apply(null, fixture.content);
         return;
       },
       onComplete: function() {

commit 6cc1ee7166341efa6b5e8d862814737b2065e7fd

Revet regression from 89e721a
In the commit 89e721a there were two changes. The first was fine, so this commit does not revert that change.

But the second change made in 89e721a was a major regression. It's all about calling Function.prototype.apply supplying fixture.content as the array of arguments. Inspecting the definition of that fixture.contents (line 47) reveals that the property comes from a module called file-reader that returns objects, not arrays. Function.prototype.apply is not appropriate for objects, so this commit ensures that Function.prototype.apply is not used (unless for some reason fixture.content happens to be array-like).

index.js
@@ -294,7 +294,12 @@ Benchmarked.prototype.addSuite = function(fixture) {
         cursor.write('  ' + event.target);
       },
       fn: function() {
-        file.run.apply(null, fixture.content);
+        var args = fixture.content;
+        if (Array.isArray(args[0])) {
+          file.run.apply(null, args);
+        } else {
+          file.run(args);
+        }
         return;
       },
       onComplete: function() {